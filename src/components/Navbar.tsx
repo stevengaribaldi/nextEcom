43c4bbd0bde21620f73b5a568a5784f156f88761
AcevedoEsteban <astevenaa@gmail.com>
2024-02-12 23:00:04 -0700

    navbar icon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,28 @@
-import { Link } from 'lucide-react';
-import MaxWithWrapper from './MaxWidthWrapper';
+import Link from 'next/link';
+import MaxWidthWrapper from './MaxWidthWrapper';
+import { Icons } from './Icons';
 
 const Navbar = () => {
   return (
-    <div className=" bg-white sticky z-50 top-0 inset-x-0 h-16">
+  <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
-        <MaxWithWrapper>
+        <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex items-center justify-between h-16">
               {/* TODO MOBILE NAV */}
 
-                          <div className="ml-4 flex lg:ml-0">
-                              <Link href="/">
-                                  
-                              </Link>
+              <div className="ml-0 flex lg:ml-0">
+                <Link href="/">
+                  <Icons.logo className="h-10 w-10" />
+                </Link>
+              </div>
+
+              <div className='hidden z-50 lg:ml-8 lg:block lg:self-stretch'>
+                {/* <NavItems /> */}
               </div>
             </div>
           </div>
-        </MaxWithWrapper>
+        </MaxWidthWrapper>
       </header>
     </div>
   );

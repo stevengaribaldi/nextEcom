516902cfd1e78cbe67674889e1d2ab92dfd3bdf2
AcevedoEsteban <astevenaa@gmail.com>
2024-03-01 01:18:00 -0700

    navbar item

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,11 +1,26 @@
 'use client';
 import { useState } from 'react';
+import { PRODUCT_CATEGORIES } from './config';
+import NavItem from './NavItem';
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
   return (
     <>
-      <div className="flex h-full gap-4"></div>
+      <div className="flex h-full gap-4">
+        {PRODUCT_CATEGORIES.map((category, i) => {
+          const handleOpen = () => {
+            if (activeIndex === i) {
+              setActiveIndex(null);
+            } else {
+              setActiveIndex(i);
+            }
+          };
+          const isOpen = i === activeIndex;
+          return <NavItem />;
+        })}
+      </div>
     </>
   );
 };

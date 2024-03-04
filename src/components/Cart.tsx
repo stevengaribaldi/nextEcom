8bbd9cbeb9d1bbd3b0de0157e5130292454bab4a
AcevedoEsteban <astevenaa@gmail.com>
2024-03-04 14:21:16 -0700

    format pricing

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -10,9 +10,10 @@ import {
   SheetTrigger,
 } from './ui/sheet';
 import { Separator } from './ui/separator';
+import { formatPrice } from '@/lib/utils';
 
 const Cart = () => {
-  const itemCount = 0;
+  const itemCount = 120;
   return (
     <>
       <div className="relative flex items-center justify-center">
@@ -21,35 +22,46 @@ const Cart = () => {
             <div className="relative flex items-center justify-center h-20 w-20">
               <ShoppingCart
                 className="absolute text-gray-400 group-hover:text-gray-100"
-                style={{ top: '-9px' }}
+                style={{ top: '-10px' }}
                 size={58}
               ></ShoppingCart>
               <span
                 style={{ right: 'calc(1.5rem + 0.04rem)' }}
-                className="text-lg font-bold absolute top-2 right-6 h-5 w-6 rounded-full text-gray-100  flex items-center justify-center "
+                className="text-sm absolute top-1.5 right-6 h-5 w-6 rounded-full text-gray-100  flex items-center justify-center "
               >
                 {itemCount}
               </span>
             </div>
           </SheetTrigger>
           <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
-            <SheetHeader className="space-y-2.5 pr-6">
+            <SheetHeader className="space-y-2.5 items-center pr-6">
               <SheetTitle> Cart({itemCount})</SheetTitle>
             </SheetHeader>
             {itemCount > 0 ? (
               <>
-                <div className="flex w-fu; flex-col pr-6">
+                <div className="flex w-fu flex-col pr-6">
                   {/* TODO: cart items logic */}
                   cart items
                 </div>
 
                 <div className="space-y-4 pr-6">
                   <Separator />
-                  <div className="space-y-1.5 pr-6"></div>
+                  <div className="space-y-1.5 text-sm">
+                    <div className="flex">
+                      <span className="flex-1">Shipping</span>
+                      <span>Free</span>
+                    </div>
+                  </div>
+                  <div className="space-y-1.5 text-sm">
+                    <div className="flex">
+                      <span className="flex-1">Transaction Fee</span>
+                      <span>{formatPrice(1)}</span>
+                    </div>
+                  </div>
                 </div>
               </>
             ) : (
-              <div ></div>
+              <div></div>
             )}
           </SheetContent>
         </Sheet>

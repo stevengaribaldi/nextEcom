8ebfcf255a3a1b3cbe526c0335f8566dae35409f
AcevedoEsteban <astevenaa@gmail.com>
2024-03-05 17:55:13 -0700

    server payload logic

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -1,2 +1,40 @@
-import dotenv from 'dotenv'
-export const getPayloadClient = async () => { };
+import dotenv from 'dotenv';
+import path from 'path';
+import type { InitOptions } from 'payload/config';
+import payload from 'payload';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+let cached = (global as any).payload;
+if (!cached) {
+  cached = (global as any).payload = {
+    client: null,
+    promise: null,
+  };
+}
+
+interface Args {
+  InitOptions?: Partial<InitOptions>;
+}
+export const getPayloadClient = async ({ InitOptions }: Args = {}) => {
+  if (!process.env.PAYLOAD_SECRET) {
+    throw new Error('Payload secret is MISSING');
+  }
+  if (cached.client) {
+    return cached.client;
+  }
+  if (!cached.promise) {
+    cached.promise = payload.init({
+      secret: process.env.PAYLOAD_SECRET,
+      local: InitOptions?.express ? false : true,
+      ...(InitOptions || {}),
+    });
+  }
+  try {
+    cached.client = await cached.promise;
+  } catch (e: unknown) {
+    cached.promise = null;
+    console.log('cached promise error', e);
+    throw e;
+  }
+};
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from 'express';
+import { getPayloadClient } from './get-payload';
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000;
 
 const start = async () => {
-  const payload = await getPayloadClient();
+  const payload = await getPayloadClient({
+    InitOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`Admin URL ${cms.getAdminURL()} `);
+      },
+    },
+  });
+  
 };
 start();

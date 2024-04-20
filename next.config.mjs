27469f658d2b3453c363fa4907010381cfc5153c
AcevedoEsteban <astevenaa@gmail.com>
2024-04-20 01:38:47 -0700

    []chor:domain name for production added

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'yehfedra.com',
-      },
+    domains: [
+      'localhost',
+      'yehfedra.com',
     ],
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;

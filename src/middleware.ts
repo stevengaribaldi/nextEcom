8a1ec34ac636c5d4024c4c556d3f15089f9aa4de
AcevedoEsteban <astevenaa@gmail.com>
2024-04-20 22:01:29 -0700

    [backend] added cache control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSideUser } from './lib/payload-utils';
 
+function setCacheHeaders(response: NextResponse) {
+
+  response.headers.set('Cache-Control', 'public, max-age=31536000, immutable');
+}
+
 export async function middleware(req: NextRequest) {
   const { nextUrl, cookies } = req;
   const { user } = await getServerSideUser(cookies);
-  if (user && ['/login', 'create-account'].includes(nextUrl.pathname)) {
+
+  if (user && ['/login', '/create-account'].includes(nextUrl.pathname)) {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`);
   }
-  return NextResponse.next();
+
+  const response = NextResponse.next();
+
+  if (nextUrl.pathname.startsWith('/_next/static/')) {
+    setCacheHeaders(response);
+  }
+
+  return response;
 }
